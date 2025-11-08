81af295de787786cbb75ac135d1c497c9e7b1835
Divya1-b <mail2divya.bathini@gmail.com>
2025-11-08 11:51:31 +0530

    inital commit

--- eclipse-workspace/FileHandling/src/test/java/MathOpsTest.java
+++ eclipse-workspace/FileHandling/src/test/java/MathOpsTest.java
@@ -6,11 +6,11 @@ public class MathOpsTest {
 		int i=2;
 		int j=3;
 		MathOperations  obj = new MathOperations();
-		assertEquals(obj.add(i, j),40);
+		assertEquals(obj.add(i, j),5);
 	}
 	@Test
 	public void testName() {
-		String name="James";
+		String name="james";
 		MathOperations obj = new MathOperations();
 		assertTrue(obj.validateName(name));
 	}
