af64b39feec3af4e8907676d77e662728806f658
Divya1-b <mail2divya.bathini@gmail.com>
2025-11-08 10:41:55 +0530

    Descriptive commit message

--- eclipse-workspace/FileHandling/src/main/java/com/app/process1/Log.java
+++ eclipse-workspace/FileHandling/src/main/java/com/app/process1/Log.java
@@ -6,7 +6,13 @@ import org.apache.logging.log4j.Logger;
 public class Log {
 	private static  final Logger logger = LogManager.getLogger();
 	public static void info(String message) {
-		logger.info();
+		logger.info(message);
+	}
+	public static void warn(String message) {
+		logger.warn(message);
+	}
+	public static void debug(String message) {
+		logger.debug(message);
 	}
 
 }
